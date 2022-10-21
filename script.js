c569386b95dfc8e32bc061aea4bdc215681d633b
Alk3bab <aliyurisherif@gmail.com>
2022-10-21 13:18:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function opdracht71(){
+const budget = 100
+var product = 60
+if(budget>product){
+    alert("u heeft genoeg geld!")
+console.log('U heeft genoeg geld!')
+}
+
+else{
+console.log("u heeft niet genoeg geld")
+alert("u heeft niet genoeg geld")
+}
+}
